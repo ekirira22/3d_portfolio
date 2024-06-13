596719a0f979d278e991788c7282ff521dd0e6d1
Eric Maranga <ekirira22@gmail.com>
2024-06-13 13:12:58 +0300

    added rotational and 3D movement to our pheonix bird

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,11 +1,21 @@
 import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+
 import skyScene from '../assets/3d/sky.glb'
 
-const Sky = () => {
-  const sky = useGLTF(skyScene)
 
+const Sky = ({ isRotating }) => {
+  const sky = useGLTF(skyScene)
+  const skyRef = useRef()
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += delta * 0.05
+    }
+  })
   return (
-    <mesh>
+    <mesh ref={skyRef}>
         {/* This will render the 3D model of the sky */}
         {/* We use a 3D sky object and not a static image so that when we rotate the clouds need to rotate as well */}
         <primitive object={sky.scene} />
